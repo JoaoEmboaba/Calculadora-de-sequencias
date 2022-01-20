11eb32bea047d4637ef0722dca50b671494cb4bc
João <84819469+JoaoVitor83@users.noreply.github.com>
2022-01-20 13:21:41 -0300

    Código

--- ProjetoDeFato.cpp
+++ ProjetoDeFato.cpp
@@ -4,12 +4,12 @@ int main () {
     float primeiroTermo;
     float ultimoTermo;
 
-std :: cout << "Opa!, Digite o primeiro termo da sequência:" << endl;
+cout << "Opa!, Digite o primeiro termo da sequência:" << endl;
 cin >> primeiroTermo;
 
-std :: cout << "Digite o último termo da sequência:" << endl;
+cout << "Digite o último termo da sequência:" << endl;
 cin >> ultimoTermo;
 
-std :: cout << "\nA soma dessa sequência é:"<<(primeiroTermo+ultimoTermo)*ultimoTermo/2 << endl;
+cout << "\nA soma dessa sequência é:"<<(primeiroTermo+ultimoTermo)*ultimoTermo/2 << endl;
 
 }
